b5544327c94830cab2d843eb6f5b6fd5f023d36e
Ranjan Khadka <khadkaranjan5@gmail.com>
2023-07-20 15:56:58 +0545

    Conditional Rendering

--- src/App.vue
+++ src/App.vue
@@ -1,80 +1,32 @@
 
 <template>
-<div>
-  {{ greet }} {{ name }}
-</div>
-<div v-html="company"></div>
-<div v-html="hack"></div>
-<h2 v-bind:id="headingId">Heading</h2>
-<button v-bind:disabled="isDisabled">Bind</button>
-<div class="underline">Lorem ipsum dolor sit amet</div>
-<div class="unserline" v-bind:class="status">Lorem, ipsum dolor.</div>
-<div v-bind:class="isPromoted && 'promoted' ">Promote Movie</div>
+<h2 v-if=" num == 0">The number is zero</h2>
+<h2 v-else-if=" num < 0">The number is negative</h2>
+<h2 v-else-if=" num > 0">The number is positive</h2>
+<h2 v-else>Not a number</h2>
 
-<div v-bind:class="isSoldout ? 'soldout' : 'new'">Movie Sold Out</div>
-
-<h2 v-bind:class="['new','promoted']">Newly Promoted</h2>
-
-<h2 v-bind:class="{
-  promoted: isPromoted,
-  new: !soldout,
-  'soldout': isSoldout
-}">
-Object Conditional
-</h2>
-
-<h2 v-bind:style="{
-  color: highlight,
-  fontSize: headerSize + 'px'
-}">Inline Style</h2>
-
-<h2 :style="styleObj">New CSS style</h2>
-
-<div :style="[baseStyleObject,successStyleObject]">Array Style</div>
-
-
-
+<template v-if="display">
+  <h2>Hello</h2>
+  <h2>World</h2>
+</template>
 
 </template>
 
 
 <script>
 
-import { ref } from 'vue';
 
 export default {
   name: 'App',
   data() {
     return {
-      greet: 'Hello',
-      name: 'Dadey',
-      company: '<b>Ambition Guru<b>',
-      hack: `<a href="#" onclick="alert('You have been hacked')">Win a prize!</a>`,
-      headingId: 'heading',
-      isDisabled: true,
-      status: 'danger',
-      isPromoted: true,
-      isSoldout: false,
-      highlight: 'blue',
-      headerSize: 50,
-      styleObj: {
-        color: 'red',
-        fontSize: '50px',
-        padidng: '20px',
-      },
-      baseStyleObject: {
-        fontSize: '50px',
-        padidng: '10px',
-      },
-      successStyleObject: {
-        color: 'green',
-        backgroundColor: 'lightgreen',
-        border: '1px solid green',
+      num : 10,
 
-      },
-    };
-  },
-};
+      display: true
+      }
+    },
+  }
+
 </script>
 
 
@@ -86,20 +38,4 @@ export default {
   margin-top: 60px;
 }
 
-.underline {
-  text-decoration: underline dashed;
-}
-
-.promoted {
-  font-style: italic;
-  font-weight: bold;
-}
-
-.soldout{
-  color: red;
-}
-
-.new{
-  color: green;
-}
 </style>
