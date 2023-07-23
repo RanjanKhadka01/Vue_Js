62710bd33f5f8988448f0e06d09078c3a84c7bcd
RanjanKhadka01 <khadkaranjan5@gmail.com>
2023-07-23 17:14:51 +0545

    Computed Properties

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,25 @@
 
 <template>
-<div>
- <h2 v-once>{{name}}</h2> <!--element in v-once only evaluate and render once-->
-  <button @click="name = 'Murra'">Change Name</button> <!--v-pre tell vue to skip the compilation process element and its children-->
 
-  <h2 v-pre>{{name}}</h2>
-</div>
+<h2>fullName: {{firstName}} {{lastName}}</h2>
+ <h2>Computed FullName: {{fullName}}</h2>
+<button @click="items.push({id:4, title: 'Keyboard', price:50})">Add Item</button>
+<h2>Computed Total : {{total}}</h2>
+<h2>Methods Total : {{getTotal()}}</h2>
 
+<input type="text" v-model="country">
+
+<template v-for="item in items" :key="item.id">
+<h2 v-if="item.price >= 100">{{item.name}} : {{item.price}}</h2>
 </template>
 
+<!-- <h2 v-for="item in expensiveItem" :key="item.id">
+  {{ item.name }}  {{ item.price }}
+</h2> -->
+
+<button @click="changeName()">Change Name</button>
+
+</template>
 
 <script>
 
@@ -18,13 +29,56 @@ export default {
   name: 'App',
   data() {
     return {
-     name: 'dadey',
+      firstName: 'Murra',
+      lastName: 'dadey',
+
+      items: [
+        {
+          id: 1,
+          name: 'apple',
+          price: 100,
+        },
+        {
+          id: 2,
+          name: 'mango',
+          price: 150,
+        },
+        {
+          id: 3,
+          name: 'banana',
+          price: 50,
+        },
+      ],
+      country: ''
       }
     },
     methods: {
-      submitForm(event) {
-        console.log('Form Values', this.fromValue)
+      getTotal() {
+        return this.items.reduce((total, curr) => (total = total + curr.price), 0)
+      },
+      changeName() {
+        this.fullName = 'Dadey Mungrati'
       }
+    },
+
+    computed: {
+      fullName: {
+        get() {
+        return `${this.firstName} ${this.lastName}`
+        },
+        set(value) {
+          const names = value.split(' ')
+          this.firstName = names[0]
+          this.lastName = names[1]
+
+        }
+      },
+      total() {
+        return this.items.reduce((total, curr) => (total = total + curr.price), 0)
+      },
+      expensiveItem() {
+        return $this.items.filter(item => item.price > 100)
+      },
     },
   }
 
