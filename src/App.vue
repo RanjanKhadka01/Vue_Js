d7c0bcdb0079ec9bb5c11b89964f947bebc0ae89
RanjanKhadka01 <khadkaranjan5@gmail.com>
2023-07-24 11:31:01 +0545

    Watcher in Vue Js

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,10 @@
 
 <template>
+<h2>Volume 0-20</h2>
+<h3>Volume: {{volume}}</h3>
+<button @click="volume +=2">Increase Volume</button>
+<button @click="volume -=2">Decrease Volume</button>
 
-<h2>fullName: {{firstName}} {{lastName}}</h2>
- <h2>Computed FullName: {{fullName}}</h2>
-<button @click="items.push({id:4, title: 'Keyboard', price:50})">Add Item</button>
-<h2>Computed Total : {{total}}</h2>
-<h2>Methods Total : {{getTotal()}}</h2>
-
-<input type="text" v-model="country">
-
-<template v-for="item in items" :key="item.id">
-<h2 v-if="item.price >= 100">{{item.name}} : {{item.price}}</h2>
-</template>
-
-<!-- <h2 v-for="item in expensiveItem" :key="item.id">
-  {{ item.name }}  {{ item.price }}
-</h2> -->
-
-<button @click="changeName()">Change Name</button>
 
 </template>
 
@@ -29,57 +16,23 @@ export default {
   name: 'App',
   data() {
     return {
-      firstName: 'Murra',
-      lastName: 'dadey',
+      volume: 0,
 
-      items: [
-        {
-          id: 1,
-          name: 'apple',
-          price: 100,
-        },
-        {
-          id: 2,
-          name: 'mango',
-          price: 150,
-        },
-        {
-          id: 3,
-          name: 'banana',
-          price: 50,
-        },
-      ],
-      country: ''
       }
     },
     methods: {
-      getTotal() {
-        return this.items.reduce((total, curr) => (total = total + curr.price), 0)
-      },
-      changeName() {
-        this.fullName = 'Dadey Mungrati'
-      }
     },
 
     computed: {
-      fullName: {
-        get() {
-        return `${this.firstName} ${this.lastName}`
-        },
-        set(value) {
-          const names = value.split(' ')
-          this.firstName = names[0]
-          this.lastName = names[1]
-
-        }
-      },
-      total() {
-        return this.items.reduce((total, curr) => (total = total + curr.price), 0)
-      },
-      expensiveItem() {
-        return $this.items.filter(item => item.price > 100)
-      },
     },
+
+    watch: {
+      volume(newData, oldData) {
+        if(newData > oldData && newData == 16){
+          alert('Listening to a high volume may damage your hearing capacity')
+        }
+      }
+    }
   }
 
 </script>
